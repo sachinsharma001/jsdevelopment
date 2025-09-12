56edd291cd064226cc5c131ee7bce21604e9c367
sachinsharma001 <ss.sachin.sharma009@gmail.com>
2025-09-12 23:52:25 +0530

    save to local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart =JSON.parse(localStorage.getItem('cart'));
+
+if (!cart){
+  cart= [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
@@ -6,14 +9,18 @@ export let cart = [
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 2
   }
-];
+];}
 
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
+
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
-      matchingItem = cartItem
+      matchingItem = cartItem;
     }
   });
 
@@ -33,6 +40,7 @@ export function addToCart(productId) {
       quantity
     });
   }
+saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -44,4 +52,5 @@ export function removeFromCart(productId) {
     }
   });
 cart=newCart;
+saveToStorage();
 }
