57e3a5856d64dd1d4b02350b4d7bf8a903cb6272
sachinsharma001 <ss.sachin.sharma009@gmail.com>
2025-09-13 02:49:30 +0530

    created one function for quantity

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -43,6 +43,15 @@ export function addToCart(productId) {
 saveToStorage();
 }
 
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;}
+
 export function removeFromCart(productId) {
   const newCart = [];
 
