f98f3e3dba5398402cb177db0905c8c4d48bf04c
sachinsharma001 <ss.sachin.sharma009@gmail.com>
2025-10-30 23:00:37 +0530

    convert products into classes

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -16,14 +16,14 @@ products.forEach((products) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${products.rating.stars * 10}.png">
+              src="${products.getStarsUrl()}">
             <div class="product-rating-count link-primary">
               $${products.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${formatCurrency(products.priceCents)}
+           ${products.getPrice()}
           </div>
           <div class="product-quantity-container">
             <select  class="js-quantity-selector-${products.id}">
--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -44,7 +44,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${formatCurrency(matchingProduct.priceCents)}
+                 ${matchingProduct.getPrice()}
                 </div>
                 <div class="product-quantity">
                   <span>
