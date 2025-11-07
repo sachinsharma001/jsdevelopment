2062d95fee2b8739eeb8499e7d28d39d302860ad
sachinsharma001 <ss.sachin.sharma009@gmail.com>
2025-11-08 00:41:25 +0530

    learn promises and convert code into promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,11 +1,39 @@
-import {rerenderOrderSummary} from './checkout/orderSummary.js'
-import {rerenderPaymentSummary} from './checkout/paymentSummary.js'
-import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+import { rerenderOrderSummary } from './checkout/orderSummary.js'
+import { rerenderPaymentSummary } from './checkout/paymentSummary.js'
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 //  import '../data/backend-practice.js'
-loadProducts(()=>{
- renderCheckoutHeader();
- rerenderOrderSummary();
- rerenderPaymentSummary();
-});
 
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve("value1");
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        })
+
+    })
+]).then((value) => {
+    console.log(value)
+    renderCheckoutHeader();
+    rerenderOrderSummary();
+    rerenderPaymentSummary();
+})
+
+// ^^^^
+// |||| instead of call back we use promise to dea with asynchronous function
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderCheckoutHeader();
+        rerenderOrderSummary();
+        rerenderPaymentSummary();
+        })
+
+});
+*/
