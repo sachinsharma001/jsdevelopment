452e4b0538e772aa2fe9b225853864eb5e0f234f
sachinsharma001 <ss.sachin.sharma009@gmail.com>
2025-11-10 23:14:52 +0530

    convert load products from xmr to fetch

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,17 +1,18 @@
 import { rerenderOrderSummary } from './checkout/orderSummary.js'
 import { rerenderPaymentSummary } from './checkout/paymentSummary.js'
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 //  import '../data/backend-practice.js'
 
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve("value1");
-        });
-    }),
+    loadProductsFetch(),
+    // new Promise((resolve) => {
+    //     loadProducts(() => {
+    //         resolve("value1");
+    //     });
+    // }),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
